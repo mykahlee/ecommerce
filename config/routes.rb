dc17e735db86cecc756ff6d4e3f4aa3d5fb48f84
mykahlee <mykah@infascination.com>
2019-08-07 20:39:42 -0500

    created storefronts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :products 
-  root 'products#index'
+  root 'storefronts#all_items'
+  get 'categorical' => 'storefronts#items_by_category'
+  get 'branding' => 'storefronts#items_by_brand'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
